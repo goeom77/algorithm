a225a2900b69adabcecc5dbbc89c35be1670212f
goeom <109336705+goeom77@users.noreply.github.com>
2022-10-11 08:32:59 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -1,9 +1,7 @@
-# 피보나치 수1
 n = int(input())
-dp = [0]*(n+1)
-dp[1] = 1
-dp[2] = 1
-for j in range(3,n+1):
-    dp[j] = dp[j-1] + dp[j-2]
 
-print(dp[n],n-2)
+f = [1, 1]
+for i in range(2, n):
+    f += [(f[i-1]+f[i-2])]
+
+print(f[n-1], n-2)
