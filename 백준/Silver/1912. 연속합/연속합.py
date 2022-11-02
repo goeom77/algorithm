d029181e659a8f0adc8c4933995aa5c8a1a46e90
goeom <109336705+goeom77@users.noreply.github.com>
2022-11-02 17:49:20 +0900

    [Silver II] Title: 연속합, Time: 116 ms, Memory: 38548 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+result = arr[0]
+dp = arr[0]
+for i in range(1,n):
+    if arr[i] > dp+arr[i]:
+        dp = arr[i]
+    elif dp+arr[i] >0:
+        dp += arr[i]
+    else:
+        dp = arr[i]
+    if dp > result:
+        result = dp
+print(result)
