e2dff7448e9a839c321d8c4df07168d7ae371ad2
goeom <109336705+goeom77@users.noreply.github.com>
2022-12-08 12:35:16 +0900

    [Gold IV] Title: 좋다, Time: 1148 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,30 +1,9 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-array = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 result = 0
-# 병합정렬
-def merge_sort(arr):
-    if len(arr) < 2:
-        return arr
-    mid = len(arr) // 2
-    low_arr = merge_sort(arr[:mid])
-    high_arr = merge_sort(arr[mid:])
-
-    merged_arr = []
-    l = h = 0
-    while l < len(low_arr) and h < len(high_arr):
-        if low_arr[l] < high_arr[h]:
-            merged_arr.append(low_arr[l])
-            l += 1
-        else:
-            merged_arr.append(high_arr[h])
-            h += 1
-    merged_arr += low_arr[l:] # 일정 이하로 0개가 되면 안더해지니깐
-    merged_arr += high_arr[h:]
-    return merged_arr
-arr = merge_sort(array)
-# print(arr)
+arr.sort()
 # 이분탐색
 for k in range(n):
     # 다른 2개의 수의 합으로 만들어진 합이니깐 index로 접근
