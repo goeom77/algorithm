e1f1200c64f2a9fef4b83aea00f6e20710059010
goeom <109336705+goeom77@users.noreply.github.com>
2023-07-10 23:53:12 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,17 @@
+score = {}
+score['A+'] = 4.3
+score['A0'] = 4.0
+score['A-'] = 3.7
+score['B+'] = 3.3
+score['B0'] = 3.0
+score['B-'] = 2.7
+score['C+'] = 2.3
+score['C0'] = 2.0
+score['C-'] = 1.7
+score['D+'] = 1.3
+score['D0'] = 1.0
+score['D-'] = 0.7
+score['F'] = 0.0
+
+a = input()
+print(score[a])
