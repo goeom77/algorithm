74193d574c65006f3f7505d129542d790ed711b3
goeom <109336705+goeom77@users.noreply.github.com>
2023-10-31 20:25:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,23 @@
+def solution(id_list, report, k):
+    reporter = {} # 내가 신고한 사람
+    reportMe = {} # 나를 신고한 사람
+    for id in id_list:
+        reporter[id] = set()
+        reportMe[id] = set()
+
+    ban = []
+    for value in report:
+        re, banner = value.split()
+        reporter[re].add(banner)
+        reportMe[banner].add(re)
+        if len(reportMe[banner]) == k:
+            ban.append(banner)
+    # 내가 신고한 사람중 몇명이 ban되었는지 list cnt를 보여주기
+        
+    # ban된 유저
+    answer = [0]*len(id_list)
+    for i in range(len(id_list)):
+        for j in reporter[id_list[i]]:
+            if j in ban:
+                answer[i] += 1
+    return answer
