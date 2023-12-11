8f6db5994469125eba8ecfe35156a5af13563316
goeom <109336705+goeom77@users.noreply.github.com>
2023-12-11 11:37:32 +0900

    [Silver II] Title: 부분수열의 합, Time: 136 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int s;
+    static int result;
+    static int[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        result = 0;
+        // n = number of vertices
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        list = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(list);
+        // list의 부분수열의 합이 s가 되는 경우의 수를 구하시오.
+        // 우선 list의 부분수열을 모두 구한다.
+        dfs(0,0);
+        if(s == 0) {
+            result--;
+        }
+        System.out.println(result);
+
+
+    }
+    private static void dfs(int index, int sum) {
+        if(index == n) {
+            if(sum == s) {
+                result++;
+            }
+            return;
+        }
+        dfs(index+1, sum+list[index]);
+        dfs(index+1, sum);
+    }
+}
