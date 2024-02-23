47e9978b7276b39a778feebaa414fecad1b9aeb3
goeom <109336705+goeom77@users.noreply.github.com>
2024-02-23 10:22:26 +0900

    [Silver II] Title: 미로 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1347. 미로 만들기/미로 만들기.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+# 1347 미로 만들기
+# F : 앞으로 한칸 L : 왼쪽으로 90도 R : 오른쪽으로 90도
+# 시작은 남쪽을 보며있다.
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+command_length = int(input())
+commands = input()
+
+dir = 2
+pos = [0, 0]
+x_list = [0]
+y_list = [0]
+for command in commands:
+    if command == 'L':
+        dir = (dir - 1) % 4
+    elif command == 'R':
+        dir = (dir + 1) % 4
+    elif command == 'F':
+        x, y = pos
+        nx, ny = x + dx[dir], y + dy[dir]
+        x_list.append(nx)
+        y_list.append(ny)
+        pos = [nx, ny]
+
+max_x, min_x, max_y, min_y = max(x_list), min(x_list), max(y_list), min(y_list)
+N, M = max_x - min_x + 1, max_y - min_y + 1
+start_x, start_y = abs(min_x), abs(min_y)
+maze = [['#'] * M for _ in range(N)]
+for x, y in zip(x_list, y_list):
+    maze[start_x + x][start_y + y] = '.'
+
+for line in maze:
+    print("".join(line))
